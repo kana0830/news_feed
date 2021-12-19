1e2a2915c677db99c2539696f6ccb4a07fe7e580
kf0830s <kf0830s@gmail.com>
2021-12-19 13:17:37 +0900

    Add:ナビゲーションメニューの実装

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:news_feed/screens/pages/about_us_page.dart';
+import 'package:news_feed/screens/pages/head_line_page.dart';
+import 'package:news_feed/screens/pages/news_list_page.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,17 +9,42 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final _pages = [
+    HeadLinePage(),
+    NewsListPage(),
+    AboutUsPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    //TODO
     return SafeArea(
       child: Scaffold(
-        body: Container(),
+        body: _pages[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.highlight),
+              label: "トップニュース",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: "ニュース一覧",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.info),
+              label: "このアプリについて",
+            ),
+          ],
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ),
       ),
-    );
-    bottomNavigationBar:
-    BottomNavigationBar(
-      items: [],
     );
   }
 }
